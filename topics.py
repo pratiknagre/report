13f554a0198292d5913b8f933a98fe7dd5373710
N1 <pathaknishant003@gmail.com>
2023-05-12 12:01:34 -0400

    add

---
+++ topics.py
@@ -0,0 +1,9 @@
+
+
+studies in following topics=
+1. sql-joins like inner,left,right,self joins
+2. sub-query like outer query using inner query
+3. window fuctions = it is advance level of sql
+4. generate CSV file Report using python & using pandas ,matplotlib & boto3 liabraries.
+5. managing code repository using git & i am using git to version control code ,track changes & collaborate with team member.
+6. exporting CSV FILE rEPORT TO S3 bucket in AWS cloud system.
